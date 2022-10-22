fbea622c53a11b72300e0cd6942b2ad854173e93
web-dot <fedev.sudipto@gmail.com>
2022-10-22 13:55:34 +0530

    c2

--- main.cpp
+++ main.cpp
@@ -35,7 +35,76 @@ int main()
 
     //Display text with variable
     int sum = 105;
-    cout << "total sum is " << sum << " rupee";
+    cout << "total sum is " << sum << " rupee" << endl;
+
+    //Add variables togather
+    int x = 5;
+    int y = 6;
+    int total = x + y;
+    cout << total << endl;
 
 
+    //exercise
+    //create a variable and assign a value
+
+    string str = "brian";
+    cout << str << endl;
+
+    //declare multiple variables
+    int i = 5, j = 10, k = 50;
+    cout << i + j + k << endl;
+
+    //one value to multiple variables
+    int p, q, r;
+    p = q = r = 5;
+    cout << p + q + r << endl;
+
+    /*
+        C++ identifiers:
+        All C++ variables must be identified with unique names.
+        These unique names are called identifiers.
+        It is recommended to use descriptive names for understandable code
+    */
+    int minutesPerHour = 60;
+
+
+    //C++ constants
+    const int myVar = 15;
+    //myVar = 20; // error -> assignment of readonly variable
+
+
+    /*
+        C++ User Input:
+        cin is a prdefined variable that reads data from keyboard with extraction operator.
+
+        cout uses insertion operator (<<)
+        cin uses extraction operator(>>)
+    */
+
+    int n;
+    cout << "Type a number and press enter: ";
+    cin >> n; //get input from keyboard
+    cout << "you have entered " << n << endl;
+
+    //Creating a simple calculator
+
+    int a, b;
+    int add;
+    cout << "Enter a number: ";
+    cin >> a;
+    cout << "Enter another number";
+    cin >> b;
+    add = a + b;
+    cout << "Sum of " << a << " and " << b << " is " << add << endl;
+
+    /*
+        C++ basic data types
+
+        Data type       size            Description
+        boolean         1 byte          stores true or false
+        char            1 byte          stores single character/letter/number, or ASCII values
+        int             2 or 4 bytes    stores whole numbers.
+        float           4 bytes         stores fractional numbers. 7 decimal digits
+        double          8 bytes         stores fractional numbers. 15 decimal digits
+    */
 }
